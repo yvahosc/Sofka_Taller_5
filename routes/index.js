f90a6dae6361e886c08e1f4b0374090bb42667ab
yvahosc <yvahosc@unal.edu.co>
2022-05-06 18:29:52 -0500

    Creación de la documentación para el proyecto

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * Creación del controlador de ruta inicial de la aplicación.
+ */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sofka taller 5', body: 'Tutorial de NodeJS' });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/**
+ * Creación del controlador de la ruta users de la aplicación.
+ */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Usuario', body: 'Elige' +
         ' la opción a la que desees ir:'});
 });
 
+/**
+ * Creación del controlador de la ruta users/cool de la aplicación.
+ */
 router.get('/cool/', function(req, res, next) {
   res.render('index', { title: 'Hola!', body: 'Eres genial!' });
 });
